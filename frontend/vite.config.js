dcd6ef878214d965104280b509debde022a38048
Serhii-Techstarter <serhii.yeromin@tn.techstarter.de>
2025-03-04 17:44:45 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': 'http://127.0.0.1:5000',  // Замените URL на адрес вашего Python-сервера
-//     },
-//   },
-// });
-
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -31,13 +10,3 @@ export default defineConfig({
     },
   },
 });
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://127.0.0.1:5000",
-//         changeOrigin: true
-//       }
-//     }
-//   }
-// });
